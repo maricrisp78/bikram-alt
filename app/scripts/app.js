db5e86c6e1f557f01672f384891d29c32184e916
Marie Porciuncula <marieporciuncula@Maries-MacBook-Air.local>
2015-12-05 15:59:47 -0800

    commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -32,6 +32,11 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
       .otherwise({
         redirectTo: '/'
       });
